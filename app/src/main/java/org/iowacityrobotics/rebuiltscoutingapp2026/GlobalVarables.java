76700b0b14f7736e13339cd9fc858616fb84276d
Dungeon <129885284+blueofficer@users.noreply.github.com>
2025-12-15 18:58:44 -0600

    adding documaentaition

--- app/src/main/java/org/iowacityrobotics/rebuiltscoutingapp2026/GlobalVarables.java
+++ app/src/main/java/org/iowacityrobotics/rebuiltscoutingapp2026/GlobalVarables.java
@@ -1,4 +1,7 @@
 package org.iowacityrobotics.rebuiltscoutingapp2026;
 
+
+//This file is to be used to store global vars ie points
+//This file is alos to be used to store any state vars
 public class GlobalVarables {
 }
