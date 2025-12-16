02dacbd81ea08226e1837b6717a3da0a4062dc5c
Dungeon <129885284+blueofficer@users.noreply.github.com>
2025-12-15 18:51:39 -0600

    Upgradeing Compile sdk
    Andriod studio needed me to chage the sdk version to 36 from 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "org.iowacityrobotics.rebuiltscoutingapp2026"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.iowacityrobotics.rebuiltscoutingapp2026"
