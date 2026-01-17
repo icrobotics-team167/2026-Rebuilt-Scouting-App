9fc6efec1be38d8e8dc4b97aaece43401a8a3dde
Loreman105 <Bemoh10@gmail.com>
2026-01-17 16:48:55 -0600

    Bug fixing Main 1-17-2026 Ben

--- app/src/main/java/org/iowacityrobotics/rebuiltscoutingapp2026/GlobalVariables.java
+++ app/src/main/java/org/iowacityrobotics/rebuiltscoutingapp2026/GlobalVariables.java
@@ -3,8 +3,13 @@
 //These are the Global Variables.
 package org.iowacityrobotics.rebuiltscoutingapp2026;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class GlobalVariables {
     // -1 = New Match
-    //  0 = Edit Match
+    // 0 = Edit Match
     public static int objectIndex = -1;
+    public static List<Map<String, Object>> dataList = new ArrayList<>();
 }
